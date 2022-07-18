00c636d3c734788da875594fcf9098a16260651c
hossein taheri <hosseintaheri76@gmail.com>
2022-07-19 00:07:59 +0430

    add product list view and product detail view templates

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='ProductList'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -7,3 +7,9 @@ class ProductListView(generic.ListView):
     queryset = Product.objects.filter(active=True)
     context_object_name = 'products'
     template_name = 'products/product_list.html'
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'products/product_detail.html'
